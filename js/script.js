99364e803683b881e7482faf5c5a570d50e7d4ad
Natani Germann Selau Leão <natanigselau@gmail.com>
2024-07-10 13:17:50 -0300

    clinicamedic

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function initTabNav(){
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent .content");
+tabContent[0].classList.add('ativo')
+
+function activeTab(index) {
+  tabContent.forEach((content) => {
+    content.classList.remove("ativo");
+  });
+  tabContent[index].classList.add("ativo");
+}
+tabMenu.forEach((itemMenu, index) => {
+  itemMenu.addEventListener("click",  () =>{
+    activeTab(index);
+  });
+
+});
+}
+initTabNav();
